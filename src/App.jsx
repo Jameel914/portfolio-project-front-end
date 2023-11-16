600ac91fd646a2312406fbb89ceb4050616eb87c
jameel914 <jameelibrahim107@gmail.com>
2023-11-16 01:07:32 -0500

    Components created and respective component routes are setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import Navbar from "./Components/Navbar";
+import AboutPage from "./Components/AboutPage";
+import PageNotFound from "./Components/PageNotFound";
+import IndexPage from "./Components/IndexPage";
+import ShowPage from "./Components/ShowPage";
+import EditPage from "./Components/EditPage";
+import NewPage from "./Components/NewPage";
+
 function App() {
   return (
-    <>
-      <h1>Portfolio Project</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/foods" element={<IndexPage />} />
+        <Route path="/foods/new" element={<NewPage />} />
+        <Route path="/foods/:index" element={<ShowPage />} />
+        <Route path="/foods/:index/edit" element={<EditPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
