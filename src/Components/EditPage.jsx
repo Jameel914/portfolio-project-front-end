60d825ca440d7d5e53d409f001e62895e91bf141
jameel914 <jameelibrahim107@gmail.com>
2023-11-16 19:56:15 -0500

    editpage is done

--- src/Components/EditPage.jsx
+++ src/Components/EditPage.jsx
@@ -34,7 +34,133 @@ function EditPage() {
       console.log(error);
     }
   }
-  return <div>EditPage</div>;
+
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    try {
+      await axios.put(`${API}/foods/${index}`, {
+        name: name,
+        image: image,
+        calories: calories,
+        price: price,
+        is_veg: is_veg,
+        is_spicy: is_spicy,
+        description: description,
+      });
+
+      navigate(`/foods`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <>
+      <div className="m-5 w-75 px-5" style={{ border: "1px solid green" }}>
+        <h2
+          className="mt-5 text-decoration-underline"
+          style={{ color: "green" }}
+        >
+          {" "}
+          Edit Item
+        </h2>
+        <form onSubmit={handleOnSubmit}>
+          <div>
+            <label className="mt-4 fs-5 fw-bold ">Name</label>
+            <input
+              style={{ width: "80%" }}
+              className="form-control form-control-lg px-5"
+              type="text"
+              placeholder="Name of the item"
+              aria-label=".form-control-lg example"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="mt-3 fs-5 fw-bold">Image</label>
+            <input
+              style={{ width: "80%" }}
+              className="form-control form-control-lg px-5"
+              type="text"
+              placeholder="Image link"
+              aria-label=".form-control-lg example"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="mt-3 fs-5 fw-bold">Calories</label>
+            <input
+              style={{ width: "80%" }}
+              className="form-control form-control-lg px-5"
+              type="number"
+              aria-label=".form-control-lg example"
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="mt-3 fs-5 fw-bold">Price</label>
+            <input
+              style={{ width: "80%" }}
+              className="form-control form-control-lg px-5"
+              type="number"
+              aria-label=".form-control-lg example"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="mt-3 fs-5 fw-bold">Description</label>
+            <input
+              style={{ width: "80%" }}
+              className="form-control form-control-lg px-5"
+              type="text"
+              placeholder="Write the description here"
+              aria-label=".form-control-lg example"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="mt-3 fs-5 fw-bold" style={{ padding: "15px" }}>
+              Vegetarian
+            </label>
+            <input
+              type="checkbox"
+              aria-label=".form-control-lg example"
+              checked={is_veg}
+              onChange={(e) => setIs_Veg(e.target.checked)}
+            />
+          </div>
+          <div>
+            <label className=" fs-5 fw-bold" style={{ padding: "15px" }}>
+              Spicy
+            </label>
+            <input
+              type="checkbox"
+              aria-label=".form-control-lg example"
+              checked={is_spicy}
+              onChange={(e) => setIs_Spicy(e.target.checked)}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-success text-dark mt-3 mb-5"
+            >
+              EDIT ITEM
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default EditPage;
