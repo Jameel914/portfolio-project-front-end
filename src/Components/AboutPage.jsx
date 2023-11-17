c26d60ea1e67e9e474f93a5496ead3f03f495b69
jameel914 <jameelibrahim107@gmail.com>
2023-11-16 20:41:58 -0500

    aboutpage is done

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="">
+      <p className="fs-2 mt-5 px-5 ">
+        Traditional Indian food is renowned worldwide for its wonderful use of
+        herbs and spices, and its diverse range of deep-fried snacks, pastries,
+        curries, gravies, sauces, rice dishes, tandoor-cooked meats, vegetable
+        dishes, chutneys, breads and sweets. This unique approach to using
+        ingredients makes our food incredibly appealing! Our food is a
+        combination of all six tastes like sweet, sour, salty, bitter, spicy and
+        astringent.
+      </p>
+    </div>
+  );
 }
 
 export default AboutPage;
