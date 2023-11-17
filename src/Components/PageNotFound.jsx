bf3f21a846f93e31f2e72945cf7021123e8ca1d5
jameel914 <jameelibrahim107@gmail.com>
2023-11-16 20:44:19 -0500

    page not found is done

--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div>
+      <h2
+        className="d-flex justify-content-center m-5"
+        style={{ fontSize: "100px", color: "red" }}
+      >
+        Page Not Found!
+      </h2>
+    </div>
+  );
 }
 
 export default PageNotFound;
