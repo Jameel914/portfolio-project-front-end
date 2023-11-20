6140e3585a515a21ebe4681eb304617f9d2fde8d
jameel914 <jameelibrahim107@gmail.com>
2023-11-20 12:16:14 -0500

    sorting by name is done

--- src/Components/IndexPage.jsx
+++ src/Components/IndexPage.jsx
@@ -27,42 +27,48 @@ function IndexPage() {
 
   return (
     <div className="row row-cols-1 row-cols-md-3 g-4 m-5">
-      {food.slice(0, visible).map((item) => {
-        return (
-          <div className="col " key={item.id}>
-            <div className="card h-100 p-2">
-              <Link to={`/foods/${item.id}`} style={{ textDecoration: "none" }}>
-                <img
-                  src={item.image}
-                  className="card-img-top"
-                  alt={item.name}
-                  style={{
-                    objectFit: "cover",
-                    height: "250px",
-                  }}
-                />
-                <div className="card-body" style={{ color: "green" }}>
-                  <h5 className="card-title">{item.name}</h5>
-                  <div className="card-footer bg-transparent border-success px-1">
-                    ${item.price}
-                  </div>
-                </div>
-              </Link>
-            </div>
-            {visible < food.length && (
-              <div className="d-flex justify-content-center mt-5">
-                <button
-                  type="button"
-                  className="btn btn-success text-dark"
-                  onClick={Loadmore}
+      {food
+        .sort((a, b) => (a.name > b.name ? 1 : -1))
+        .slice(0, visible)
+        .map((item) => {
+          return (
+            <div className="col " key={item.id}>
+              <div className="card h-100 p-2">
+                <Link
+                  to={`/foods/${item.id}`}
+                  style={{ textDecoration: "none" }}
                 >
-                  LOAD MORE
-                </button>
+                  <img
+                    src={item.image}
+                    className="card-img-top"
+                    alt={item.name}
+                    style={{
+                      objectFit: "cover",
+                      height: "250px",
+                    }}
+                  />
+                  <div className="card-body" style={{ color: "green" }}>
+                    <h5 className="card-title">{item.name}</h5>
+                    <div className="card-footer bg-transparent border-success px-1">
+                      ${item.price}
+                    </div>
+                  </div>
+                </Link>
               </div>
-            )}
-          </div>
-        );
-      })}
+            </div>
+          );
+        })}
+      {visible < food.length && (
+        <div className="d-flex justify-content-center mt-5">
+          <button
+            type="button"
+            className="btn btn-success text-dark"
+            onClick={Loadmore}
+          >
+            LOAD MORE
+          </button>
+        </div>
+      )}
     </div>
   );
 }
